10bfa0cc7dc1059f60ff38595e61457db16d8c54
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-26 04:13:20 +0600

    Использовал eslint и prettier. Добавил элемент для показа даты, перевел кнопку и добавил символы

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,38 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Card, Col, Container, Row } from 'react-bootstrap';
-import axiosAPI from '../axiosAPI.ts';
-import { Post } from '../types';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import axiosAPI from "../axiosAPI.ts";
+import { Post } from "../types";
 
 const Home: React.FC = () => {
   const [posts, setPosts] = useState<Post[]>([]);
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await axiosAPI.get('/posts.json');
+        const response = await axiosAPI.get("/posts.json");
         const postsData = response.data;
-        const postsArray = postsData ? Object.keys(postsData).map(key => ({id: key, ...postsData[key]})) : [];
+        const postsArray = postsData
+          ? Object.keys(postsData).map((key) => ({
+              id: key,
+              ...postsData[key],
+            }))
+          : [];
         setPosts(postsArray);
       } catch (error) {
-        console.error('error fetching posts:', error);
+        console.error("error fetching posts:", error);
       }
     };
     (async () => {
       await fetchPosts();
     })();
   }, []);
+  // @ts-ignore
   return (
     <Container>
       <Row>
         <Col>
           <h1 className="text-primary my-4">Домашняя страница</h1>
-          {posts.map(post => (
+          {posts.map((post) => (
             <Card className="mb-4" key={post.id}>
               <Card.Body>
+                <Card.Text>
+                  <small className='text-muted'>Создано: {new Date(post.createdAt).toLocaleString()}</small>
+                </Card.Text>
                 <Card.Title>{post.title}</Card.Title>
                 <Card.Text>{post.body.substring(0, 100)} ...</Card.Text>
                 <Link to={`/posts/${post.id}`}>
-                  <Button variant="primary">read more</Button>
+                  <Button variant="primary">Показать больше &gt;&gt;</Button>
                 </Link>
               </Card.Body>
             </Card>
